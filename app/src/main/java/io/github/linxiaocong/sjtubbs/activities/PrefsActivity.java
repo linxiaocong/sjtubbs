a0c19cbb2f38e345d7c493fd8b7d0c6b216ff45d
Lin, Charles <charles.lin@msn.com>
2014-10-23 17:33:53 +0800

    replace PreferenceActivity with PreferenceFragment

--- app/src/main/java/io/github/linxiaocong/sjtubbs/activities/PrefsActivity.java
+++ app/src/main/java/io/github/linxiaocong/sjtubbs/activities/PrefsActivity.java
@@ -9,58 +9,20 @@ import android.util.Log;
 import android.widget.Toast;
 
 import io.github.linxiaocong.sjtubbs.R;
+import io.github.linxiaocong.sjtubbs.fragments.PrefsFragment;
 import io.github.linxiaocong.sjtubbs.utilities.BBSUtils;
 
 /**
  * Created by linxiaocong on 2014/10/21.
  */
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends SingleFragmentActivity {
 
-    /*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().setTitle(R.string.action_settings);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, (new PreferencesFragment()))
+        getSupportActionBar().setTitle(R.string.action_settings);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, (new PrefsFragment()))
                 .commit();
-    }
-    */
-
-    private static final String tag = "PrefsActivity";
-    private SharedPreferences mSharedPreferences;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mSharedPreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-                String username = sharedPreferences.getString(getResources().getString(R.string.key_username),
-                        "");
-                String password = sharedPreferences.getString(getResources().getString(R.string.key_password),
-                        "");
-                Log.d(tag, "Username: " + username);
-                AsyncTask<String, Void, Boolean> loginTask = new AsyncTask<String, Void, Boolean>() {
-                    @Override
-                    protected Boolean doInBackground(String... params) {
-                        return BBSUtils.getInstance().login(params[0], params[1]);
-                    }
-                    @Override
-                    protected void onPostExecute(Boolean result) {
-                        if (result == Boolean.TRUE) {
-                            Toast.makeText(PrefsActivity.this, R.string.info_login_successfully,
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(PrefsActivity.this, R.string.error_wrong_password,
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                };
-                loginTask.execute(username, password);
-            }
-        });
     }
 }
---
+++ app/src/main/java/io/github/linxiaocong/sjtubbs/fragments/PrefsFragment.java
@@ -0,0 +1,60 @@
+package io.github.linxiaocong.sjtubbs.fragments;
+
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import io.github.linxiaocong.sjtubbs.R;
+import io.github.linxiaocong.sjtubbs.utilities.BBSUtils;
+
+/**
+ * Created by linxiaocong on 2014/10/21.
+ */
+public class PrefsFragment extends PreferenceFragment {
+
+    private SharedPreferences mSharedPreferences;
+    private static final String tag = "PrefsFragment";
+
+    PreferenceFragment  newInstance() {
+        return new PrefsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                String username = sharedPreferences.getString(getResources().getString(R.string.key_username),
+                        "");
+                String password = sharedPreferences.getString(getResources().getString(R.string.key_password),
+                        "");
+                Log.d(tag, "Username: " + username);
+                AsyncTask<String, Void, Boolean> loginTask = new AsyncTask<String, Void, Boolean>() {
+                    @Override
+                    protected Boolean doInBackground(String... params) {
+                        return BBSUtils.getInstance().login(params[0], params[1]);
+                    }
+                    @Override
+                    protected void onPostExecute(Boolean result) {
+                        if (result == Boolean.TRUE) {
+                            Toast.makeText(getActivity(), R.string.info_login_successfully,
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), R.string.error_wrong_password,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+                loginTask.execute(username, password);
+            }
+        });
+    }
+}
