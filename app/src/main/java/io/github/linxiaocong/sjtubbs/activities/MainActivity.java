551fe43a42f89dc4e1034fa2c9fa06bcb5658cda
Lin, Charles <charles.lin@msn.com>
2014-10-23 20:48:55 +0800

    modify the colors

--- app/src/main/java/io/github/linxiaocong/sjtubbs/activities/MainActivity.java
+++ app/src/main/java/io/github/linxiaocong/sjtubbs/activities/MainActivity.java
@@ -1,13 +1,13 @@
 package io.github.linxiaocong.sjtubbs.activities;
 
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
