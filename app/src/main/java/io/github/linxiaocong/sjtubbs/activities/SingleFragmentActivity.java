b45d2076e28e14640f357fc47b6ac481ed8634d3
Lin, Charles <charles.lin@msn.com>
2014-10-24 22:26:14 +0800

    remove unused import

--- app/src/main/java/io/github/linxiaocong/sjtubbs/activities/SingleFragmentActivity.java
+++ app/src/main/java/io/github/linxiaocong/sjtubbs/activities/SingleFragmentActivity.java
@@ -1,6 +1,5 @@
 package io.github.linxiaocong.sjtubbs.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
